70d6cc4d5ebd69e1c16944ec4c654b76ed065895
Newadii <gloznek.t@gmail.com>
2018-10-06 03:20:45 +0200

    v1 testing
    Signed-off-by: Newadii <gloznek.t@gmail.com>

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
-#include <stdio.h>
+// zadanie1.c -- Tomáš Gloznek, 24.9.2018 11:15
 
-int main() {
-    for(int i=0; i<10; i++)
-        printf("Hello, World!\n");
+#include <stdio.h>
+#include <string.h>
+
+void **head;
+
+void *memory_alloc(unsigned int size)
+{
+    void *next = *head;
+    return next;
+}
+
+int memory_free(void *valid_ptr)
+{
+    return 0;
+}
+
+int memory_check(void *ptr)
+{
+    return 0;
+}
+
+void memory_init(void *ptr, unsigned int size)
+{
+    head = ptr;
+    *((double**)head) = (double*)head+1;
+}
+
+// Vlastna funkcia main() je pre vase osobne testovanie. Dolezite: pri testovacich scenaroch sa nebude spustat!
+int main()
+{
+    char region[50];
+    memory_init(region, 50);
+    char* pointer = (char*) memory_alloc(10);
+    if (pointer)
+        memset(pointer, 8, 10);
+    if (pointer)
+        memory_free(pointer);
+    printf("\nHello World\n");
+    printf("size of: %d", (int)sizeof(double));
     return 0;
 }
